8ca51274aa811f5ca1968344d9b47f22be36efd5
wuchengye <wuchengye@cmos.chinamobile.com>
2020-08-10 17:37:05 +0800

    华为官网详情数据获取

--- spiderman/src/main/java/com/jingdong/webmagic/PageProcessor/OfficialDetailPageProcessor.java
+++ spiderman/src/main/java/com/jingdong/webmagic/PageProcessor/OfficialDetailPageProcessor.java
@@ -4,6 +4,7 @@ import com.jingdong.webmagic.Downloader.ChromeDownloader;
 import com.jingdong.webmagic.Model.ItemEntity;
 import com.jingdong.webmagic.Model.PriceEntity;
 import com.jingdong.webmagic.Utils.CommonUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,8 @@ public class OfficialDetailPageProcessor implements PageProcessor {
             .addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
             .setRetryTimes(3);
     private WebDriver driver;
+    List<ItemEntity> itemEntityList = new ArrayList<>();
+    List<PriceEntity> priceEntityList = new ArrayList<>();
 
     @Override
     public void process(Page page) {
@@ -45,20 +48,82 @@ public class OfficialDetailPageProcessor implements PageProcessor {
         }
     }
 
-    private void doHUAWEI(Page page) {
-        List<ItemEntity> itemEntityList = new ArrayList<>();
-        List<PriceEntity> priceEntityList = new ArrayList<>();
-
+    private void doHUAWEI(Page page) throws InterruptedException {
         List<String > colors = page.getHtml().xpath("div [@id='pro-skus']/dl [@class='product-choosepic']//li/@class").all();
-        for(int index = 0; index < colors.size(); index ++){
-            if(colors.get(index).contains("selected")){
-                colors.set(index,colors.get(index).replace(" selected",""));
+        List<String> specs = page.getHtml().xpath("div [@id='pro-skus']/dl [@class='product-choose clearfix']//li [@data-attrname='版本']/@class").all();
+        if(colors != null && colors.size() > 0){
+            for(int index = 0; index < colors.size(); index ++){
+                if(colors.get(index).contains("selected")){
+                    colors.set(index,colors.get(index).replace(" selected",""));
+                }
+                try {
+                    driver.findElement(By.xpath("//div [@id='pro-skus']/dl [contains(@class,'product-choosepic')]//li [contains(@class,'" + colors.get(index)+ "')]")).click();
+                }catch (Exception e){
+                    System.out.println("yanse");
+                }
+                Thread.sleep(5 * 1000);
+                if(specs != null && specs.size() > 0){
+                    for(int index2 = 0;index2 < specs.size();index2 ++){
+                        if(specs.get(index2).contains("selected")){
+                            specs.set(index2,specs.get(index2).replace(" selected",""));
+                        }
+                        try {
+                            driver.findElement(By.xpath("//div [@id='pro-skus']/dl [contains(@class,'product-choose')]//li [@data-attrname='版本' and contains(@class,'" + specs.get(index2) + "')]")).click();
+                        }catch (Exception e){
+                            System.out.println("banben");
+                        }
+                        Thread.sleep(5 * 1000);
+                        page.clearHtml();
+                        page.setRawText(driver.getPageSource());
+                        getHuaWeiInfo(page);
+                    }
+                }else {
+                    page.clearHtml();
+                    page.setRawText(driver.getPageSource());
+                    getHuaWeiInfo(page);
+                }
+            }
+        }else if(specs != null && specs.size() > 0){
+            for(int index2 = 0;index2 < specs.size();index2 ++) {
+                if (specs.get(index2).contains("selected")) {
+                    specs.set(index2, specs.get(index2).replace(" selected", ""));
+                }
+                try {
+                    driver.findElement(By.xpath("//div [@id='pro-skus']/dl [@class='product-choose clearfix']//li [@data-attrname='版本' and @class='" + specs.get(index2) + "']"));
+                }catch (Exception e){
+
+                }
+                Thread.sleep(5 * 1000);
+                page.clearHtml();
+                page.setRawText(driver.getPageSource());
+                getHuaWeiInfo(page);
             }
         }
-        page.putField("key",page.getHtml().xpath("div [@id='choose-attr-2']//div [@class='item  selected']//a/text()"));
-        System.out.println("xxxx" + page.getHtml().xpath("div [@id='choose-attr-2']//div [@class='item  selected']//a/text()").toString());
-        for (String c: colors){
-            System.out.println(c);
+    }
+
+    private void getHuaWeiInfo(Page page) {
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setItemFullName(page.getHtml().xpath("h1 [@id='pro-name']/text()").toString());
+        itemEntity.setItemId(Long.valueOf(page.getHtml().xpath("div [@id='pro-sku-code2']/text()").toString()));
+        itemEntity.setBrand(CommonUtils.regexBrand(itemEntity.getItemFullName()));
+        itemEntity.setColor(page.getHtml().xpath("div [@id='pro-skus']/dl [@class='product-choosepic']//li [@class='selected']//span/text()").toString());
+        itemEntity.setSpecs(page.getHtml().xpath("div [@id='pro-skus']/dl [@class='product-choose clearfix']//li [@data-attrname='版本' and @class='selected']//span/text()").toString());
+        itemEntity.setFormat(CommonUtils.regexFormat(itemEntity.getItemFullName()));
+        itemEntity.setItemUrl(page.getUrl().get());
+        itemEntity.setModel(page.getHtml().xpath("div [@class='product-parameter-main clearfix hide' and @style='display: block;']/ul/li[1]/span/text()").toString());
+        itemEntityList.add(itemEntity);
+        PriceEntity priceEntity = new PriceEntity();
+        priceEntity.setPrice(Double.valueOf(page.getHtml().xpath("div [@class='product-price-info']/span [@id='pro-price']/text()").toString()));
+
+        List<String> gifts = page.getHtml().xpath("div [@id='product-prom-all']/div [@class='product-prom-item clearfix']/div/span/text()").all();
+        if(gifts != null && gifts.size() > 0){
+            StringBuffer sb = new StringBuffer();
+            for (String s : gifts){
+                sb.append(s);
+            }
+            priceEntity.setHaveGift("是");
+        }else {
+            priceEntity.setHaveGift("否");
         }
     }
 
